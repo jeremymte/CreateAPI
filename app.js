8fe4b2629eda40823cd117ea1cfca15a08be3729
Jeremy Te <jayte718@gmail.com>
2019-11-12 20:18:59 -0500

    Connected MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 //Middlewares
 // app.use('/posts', () => {
@@ -14,9 +21,12 @@ app.get('/', (req, res) => {
 
 
 
-app.get('/posts', (req, res) => {
-    res.send('We are on posts');
-});
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+    console.log('Connected to DB!')
+);
 
 //How do we start listening to the server
 app.listen(3001);
