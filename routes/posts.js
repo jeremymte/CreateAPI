ce3b5a56be769cfc1d811f73f420a8fb9298c817
Jeremy Te <jayte718@gmail.com>
2019-11-14 18:59:42 -0500

    test get and posts with Postman

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
+
+
+//GETS BACK ALL THE POSTS
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
 router.get('/specific', (req, res) => {
@@ -11,5 +19,28 @@ router.get('/specific', (req, res) => {
 });
 
 
+//SUBMITS A POST
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    })
+    try {
+        const savedPost = await post.save()
+        res.json(savedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+})
+
+//SPECIFIC POST
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post)
+    } catch (err) {
+        res.json({ message: err });
+    }
+})
 
 module.exports = router;
