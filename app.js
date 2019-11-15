ff0a12503f188615c1d26d9c41e89880a8c37aab
Jeremy Te <jayte718@gmail.com>
2019-11-14 19:37:51 -0500

    finalized posts js

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
+app.use(cors());
 app.use(bodyParser.json());
 
 //Import Routes
@@ -20,10 +22,6 @@ app.use('/posts', postsRoute);
 app.get('/', (req, res) => {
     res.send('We are on home');
 });
-
-
-
-
 
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
--- routes/posts.js
+++ routes/posts.js
@@ -31,13 +31,35 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.json({ message: err });
     }
-})
+});
 
 //SPECIFIC POST
 router.get('/:postId', async (req, res) => {
     try {
         const post = await Post.findById(req.params.postId);
         res.json(post)
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+//DELETE POST
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId })
+        res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+//UPDATE POST
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne({ _id: req.params.postId },
+            { $set: { title: req.body.title } }
+        );
+        res.json(updatedPost);
     } catch (err) {
         res.json({ message: err });
     }
